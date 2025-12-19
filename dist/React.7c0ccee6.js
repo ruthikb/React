013585540d84ff5d242afc2a2fa2d77765aa2c41
kruthik693 <kruthik693@gmail.com>
2025-12-19 12:21:46 +0530

    chore: initialize package.json with basic configuration and dependencies for React project

---
+++ dist/React.7c0ccee6.js
@@ -0,0 +1,19 @@
+// import React from "react";
+// import ReactDOM from "react-dom";
+// const element = React.createElement(
+//   "div",
+//   null,
+//   React.createElement(
+//     "div",
+//     null,
+//     React.createElement(
+//       "div",
+//       null,
+//       React.createElement("h1", null, "Hello from React")
+//     )
+//   )
+// );
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(element);
+
+//# sourceMappingURL=React.7c0ccee6.js.map
