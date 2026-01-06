917384457f0d33a208ccbf936ccaded7ad4ebb94
kruthik693 <kruthik693@gmail.com>
2026-01-06 11:52:52 +0530

    Worked on the useState function using hooks

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -1,15 +1,31 @@
+import { useState } from "react";
 import { restaurantData } from "../Utility/Constant.jsx";
 import { Restaurant } from "./Restaurant.jsx";
 
 export var Body = () => {
+  //state variable to hold the data
+  const [listOfRestaurants, setListOfRestaurants] = useState(restaurantData);
   return (
     <div className="body">
       <div className="search">
         <input type="text" placeholder="Search here"></input>
         <button>Search</button>
       </div>
+      <div className="filter">
+        <button
+          onClick={() => {
+            const filteredList = listOfRestaurants.filter(
+              (res) => res.card.card.info.avgRating > 4.5
+            );
+            setListOfRestaurants(filteredList);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
+
       <div className="restaurant-list">
-        {restaurantData.map((rest) => (
+        {listOfRestaurants.map((rest) => (
           <Restaurant key={rest.card.card.info.id} restObj={rest} />
         ))}
       </div>
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,8 +1,11 @@
+import { useState } from "react";
+
 export var Header = () => {
+   const [button, setButton] = useState("login");
   return (
     <div className="Outerheader">
       <div className="logo">
-        <img src="https://i.pinimg.com/originals/b1/fc/bb/b1fcbbfd4fb8116c714ef352bb39bbaf.jpg" style={{ width: "150px" }}></img>
+        <img src="https://i.pinimg.com/originals/b1/fc/bb/b1fcbbfd4fb8116c714ef352bb39bbaf.jpg" style={{ width: "150px" }} />
       </div>
       <div className="nav-items">
         <ul>
@@ -10,6 +13,20 @@ export var Header = () => {
           <li>About</li>
           <li>Contact</li>
           <li>Cart</li>
+          <li>
+            <button
+              onClick={() => {
+                if (button == "login") {
+                  setButton("logout");
+                } else {
+                  setButton("login");
+                }
+              }}
+              className="login-button s"
+            >
+              {button}
+            </button>
+          </li>
         </ul>
       </div>
     </div>
